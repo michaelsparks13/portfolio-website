c4935bac723a3299f2ce0b9736abd3f9f35b0644
Michael Sparks <michael.sparks13@gmail.com>
2022-03-10 21:23:32 -0500

    updated stylings of nat ev trkr

--- eng-projects/natural-event-tracker/src/components/Header.js
+++ eng-projects/natural-event-tracker/src/components/Header.js
@@ -1,9 +1,9 @@
-
+import nasa_logo from './nasa_logo.png'
 
 function Header() {
     return (
         <header className="header">
-            <h1>Natural Event Tracker (Data Provided by NASA)</h1>
+            <h1>Natural Event Tracker: Data Provided by</h1><img src={nasa_logo} alt="nasa logo"/>
         </header>
     )
 }

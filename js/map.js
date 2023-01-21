e00984ecece6222194fa78d90baac7bfff6000b3
Michael Sparks <115479949+michael-sparks13@users.noreply.github.com>
2023-01-21 11:19:27 -0500

    get markers working

--- js/map.js
+++ js/map.js
@@ -1,43 +1,63 @@
- // center map on cape town
- let options = {
-    center: [38.53784243091848, -100.76566617010869],
-    zoom: 2,
-  };
+import {projects} from './projects.js'
 
-  let map = L.map("map", options);
 
-  //   create map details
-  let basemap_url =
-    "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}";
+// center map on cape town
+let options = {
+  center: [38.53784243091848, -100.76566617010869],
+  zoom: 2,
+};
 
-  let basemap_attributes = {
-    attribution:
-      'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    subdomains: "abcd",
-    minZoom: 0,
-    maxZoom: 20,
-    ext: "png",
-  };
+let map = L.map("map", options);
 
-  let tiles = L.tileLayer(basemap_url, basemap_attributes).addTo(map);
+//   create map details
+let basemap_url =
+  "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}";
 
-  let message = "Cape Town, Western Cape, SA";
+let basemap_attributes = {
+  attribution:
+    'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  subdomains: "abcd",
+  minZoom: 0,
+  maxZoom: 20,
+  ext: "png",
+};
 
-  const cityName = "Cape Town",
-    cityCoords = [-33.885513604234575, 18.454849596109685];
+let tiles = L.tileLayer(basemap_url, basemap_attributes).addTo(map);
 
-    // create the Leaflet marker using the coordinates
-    L.marker(cityCoords)
-    .bindTooltip(cityName)
-    .addTo(map);
 
-  // add marker to map
-  var sidebar = L.control.sidebar('sidebar', {
-    position: 'left'
+// add marker to map
+var sidebar = L.control.sidebar("sidebar", {
+  position: "left",
 });
 
-map.on('click', function () {
-    sidebar.toggle();
-})
 
+function createMarkers(projects) {
+    for (let project in projects) {
+       let mkr = L.marker(projects[project].cityCoords).bindPopup(projects[project].cityName).addTo(map);
+    }
+}
+
+
+
+// for (k in maps) {
+// //   mkr = L.marker(maps[k].cityCoords).bindPopup(maps[k].cityName).addTo(map);
+
+//   mkr.on("click", function () {
+//     let sb = document.getElementById("sidebar");
+
+//     let content = `
+//    <article class="sidebar-content">
+//    <h1>${maps[k].cityName}</h1>
+//    <div class="info-content">
+//      <div class="info-description">${maps[k].cityInfo}</div>
+//      <div><a href="${maps[k].cityLink}" target="_" alt="$maps[k].cityName} map">Map</a></div>
+//    </div>
+//  </article>`;
+
+//     sb.innerHTML += content;
+//     sidebar.toggle();
+//   });
+// }
+
+createMarkers(projects);
 map.addControl(sidebar);
---
+++ js/projects.js
@@ -0,0 +1,17 @@
+const projects = {
+    "Cape Town": {
+      cityName: "Cape Town",
+      cityCoords: [-33.885513604234575, 18.454849596109685],
+      cityInfo: "a nice place",
+      cityLink: "https://michael-sparks13.github.io/cape-town/",
+    },
+  
+    Flavortown: {
+      cityName: "Flavortown",
+      cityCoords: [-3.885513604234575, -58.454849596109685],
+      cityInfo: "a nice place",
+      cityLink: "https://michael-sparks13.github.io/flavortown/",
+    },
+  };
+
+  export {projects};
